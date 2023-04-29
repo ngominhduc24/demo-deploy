a7d5023581b5497602fd76f280780d3cdca985d8
duc minh <ngominhduc100@gmail.com>
2023-04-29 22:38:55 +0700

    revert

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const app = require("express")();
-const { v4 } = require("uuid");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-
-module.exports = app;
