38cfe77e65b7adce891d740cf8073dbccf026a25
duc minh <ngominhduc100@gmail.com>
2023-04-29 23:38:11 +0700

    final

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const express = require("express");
-const app = express();
+// i want to run file server.js
 
-app.use(express.json({ extended: false }));
-
-app.use("", async (req, res) => {
-  try {
-    return res.send("Hello Duc!");
-    // res.json({
-    //   status: 200,
-    //   message: "Get data has successfully",
-    // });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
-});
+const app = require("./src/server");
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.use("", async (req, res) => {
+  try {
+    return res.send("Hello Duc!");
+    // res.json({
+    //   status: 200,
+    //   message: "Get data has successfully",
+    // });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+module.exports = app;
