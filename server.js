1ce0a0591474aa4d5ec8db757e0dad1d9c3f6ffd
duc minh <ngominhduc100@gmail.com>
2023-04-29 22:34:16 +0700

    .

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const app = require("express")();
+const { v4 } = require("uuid");
 
-app.get("/", (req, res) => {
-  res.send("Hello Minh Duc!");
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
 module.exports = app;
